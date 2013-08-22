3d1d9fb0d1b8fc7b37c47e7d3a8ef8341f30ef9a
atmosphere <atmosphere@mybox.(none)>
2013-08-22 17:03:55 +0200

    fixing static_files to settings.py

--- atmosphere/settings_general.py
+++ atmosphere/settings_general.py
@@ -47,12 +47,6 @@ MEDIA_ROOT = os.path.join(PROJ_DIR, 'media/')
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = 'media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/static/'
-
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'

62a4b38165fb9f5e6fa5028c68798900a7284af7
Scheck <david@kwyk.fr>
2013-08-26 19:28:41 +0200

    try to fix debug=true

--- blog/admin.py
+++ blog/admin.py
@@ -6,22 +6,22 @@ from modeltranslation.admin import TranslationAdmin
 
 class ArticleAdmin(admin.ModelAdmin):
     # sets values for how the admin site lists your products
-    list_display = ('title', 'author', 'created_at', 'pub_date',)
-    list_display_links = ('title', 'created_at')
+    list_display = (u'title', u'author', u'created_at', u'pub_date',)
+    list_display_links = (u'title', u'created_at')
     list_per_page = 20
-    ordering = ['-created_at']
-    search_fields = ['title', 'content', 'meta_keywords', 'meta_description']
+    ordering = [u'-created_at']
+    search_fields = [u'title', u'content', u'meta_keywords', u'meta_description']
     # sets up slug to be generated from product title
-    prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {u'slug': (u'title',)}
 
     class Media:
 		js = (
-			'/static/tiny_mce/tiny_mce.js',
-		 	'modeltranslation/js/force_jquery.js',
-         	'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js',
-			'modeltranslation/js/tabbed_translation_fields.js',
+			u'/static/tiny_mce/tiny_mce.js',
+		 	u'modeltranslation/js/force_jquery.js',
+         	u'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js',
+			u'modeltranslation/js/tabbed_translation_fields.js',
         )
-		css ={'screen': ('modeltranslation/css/tabbed_translation_fields.css',),}
+		css = {'screen': ('modeltranslation/css/tabbed_translation_fields.css',),}
 
 
 class MyTranslatedArticleAdmin(ArticleAdmin, TranslationAdmin):

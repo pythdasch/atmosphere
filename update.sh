e757c4f01dbe74cb9c8882afe6f327f194b7a6b3
Scheck <david@kwyk.fr>
2013-08-27 11:01:03 +0200

    Revert "adding update.sh"
    This reverts commit dd4137cce1ddb09ca4a557d89ab02e4ac652f63d.

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull origin master
-source venv/bin/activate
-./venv/bin/pip install -r requirements.txt
-./manage.py collectstatic --noinput
-./manage.py migrate
-./manage.py compilemessages

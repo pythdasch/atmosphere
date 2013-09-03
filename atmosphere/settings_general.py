332a089ef8fb2470e3dde1c9b5240a9eca21f0e0
atmosphere <atmosphere@mybox.(none)>
2013-09-03 12:45:06 +0200

    changing settings_general

--- atmosphere/settings_general.py
+++ atmosphere/settings_general.py
@@ -164,32 +164,38 @@ INSTALLED_APPS = (
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-        'filters': {
-                'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse'
-                }
-         },
-         'handlers': {
-                 'mail_admins': {
-                        'level': 'ERROR',
-                        'filters': ['require_debug_false'],
-                        'class': 'django.utils.log.AdminEmailHandler'
-                },
-         'logfile': {
-                 'class': 'logging.handlers.WatchedFileHandler',
-                 'filename': '/home/david/src/atmosphere/log/django/error.log'
-         },
+LOGGING ={ 
+        'version': 1,
+        'disable_existing_loggers': True,
+   	'filters': {
+        	'require_debug_false': {
+            	'()': 'django.utils.log.RequireDebugFalse'
+        	}
+	 },
+   	 'handlers': {
+       		 'mail_admins': {
+            		'level': 'ERROR',
+            		'filters': ['require_debug_false'],
+            		'class': 'django.utils.log.AdminEmailHandler'
+        	},
+       	 'logfile': {
+           	 'class': 'logging.handlers.WatchedFileHandler',
+           	 'filename': '/home/atmosphere/log/django/error.log'
+       	 },
     },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'DEBUG',
-            'propagate': True,
+    	'loggers': {
+        	'django.request': {
+            	'handlers': ['mail_admins'],
+            	'level': 'ERROR',
+            	'propagate': True,
+        },
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
         },
     }
+
 }
 
 POST_PER_PAGE = 10

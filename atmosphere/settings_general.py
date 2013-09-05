acc97aa8cd4dee88725168aba9a9cecefe729438
Scheck <david@kwyk.fr>
2013-09-05 12:17:08 +0200

    fixing settings and compress

--- atmosphere/settings_general.py
+++ atmosphere/settings_general.py
@@ -161,44 +161,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING ={
-        'version': 1,
-        'disable_existing_loggers': True,
-   	'filters': {
-        	'require_debug_false': {
-            	'()': 'django.utils.log.RequireDebugFalse'
-        	}
-	 },
-   	 'handlers': {
-       		 'mail_admins': {
-            		'level': 'ERROR',
-            		'filters': ['require_debug_false'],
-            		'class': 'django.utils.log.AdminEmailHandler'
-        	},
-       	 'logfile': {
-           	 'class': 'logging.handlers.WatchedFileHandler',
-           	 'filename': '/home/david/src/atmosphere/log/django/error.log'
-       	 },
-    },
-    	'loggers': {
-        	'django.request': {
-            	'handlers': ['mail_admins'],
-            	'level': 'ERROR',
-            	'propagate': True,
-        },
-        'django': {
-            'handlers': ['logfile'],
-            'level': 'ERROR',
-            'propagate': False,
-        },
-    }
 
-}
 
 POST_PER_PAGE = 10
 
@@ -252,5 +215,4 @@ FILEBROWSER_ADMIN_THUMBNAIL = ('fb_thumb')
 
 
 #compress
-COMPRESS_OFFLINE = True
 COMPRESS_ENABLED = True

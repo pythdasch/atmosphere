91f8d6404a26a53c348badb13c12ba55789126c2
Scheck <david@kwyk.fr>
2013-09-17 15:57:39 +0200

    fix migrations

---
+++ gallery/migrations/0001_initial.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Author'
+        db.create_table(u'gallery_author', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('name_fr', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
+            ('name_en', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
+            ('biography', self.gf('django.db.models.fields.TextField')()),
+            ('biography_fr', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('biography_en', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+        ))
+        db.send_create_signal(u'gallery', ['Author'])
+
+        # Adding model 'Gallery'
+        db.create_table(u'gallery_gallery', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('name_fr', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('name_en', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('description_fr', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('description_en', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['gallery.Author'])),
+            ('meta_keywords', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_keywords_fr', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_keywords_en', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_description', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_description_fr', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_description_en', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'gallery', ['Gallery'])
+
+        # Adding model 'Image'
+        db.create_table(u'gallery_image', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('thumbnail', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('gallery', self.gf('django.db.models.fields.related.ForeignKey')(related_name='photos', to=orm['gallery.Gallery'])),
+        ))
+        db.send_create_signal(u'gallery', ['Image'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Author'
+        db.delete_table(u'gallery_author')
+
+        # Deleting model 'Gallery'
+        db.delete_table(u'gallery_gallery')
+
+        # Deleting model 'Image'
+        db.delete_table(u'gallery_image')
+
+
+    models = {
+        u'gallery.author': {
+            'Meta': {'object_name': 'Author'},
+            'biography': ('django.db.models.fields.TextField', [], {}),
+            'biography_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'biography_fr': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name_fr': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'gallery.gallery': {
+            'Meta': {'object_name': 'Gallery'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['gallery.Author']"}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'description_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'description_fr': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'meta_description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_description_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_description_fr': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_keywords': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_keywords_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_keywords_fr': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'name_fr': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        u'gallery.image': {
+            'Meta': {'object_name': 'Image'},
+            'gallery': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'photos'", 'to': u"orm['gallery.Gallery']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['gallery']

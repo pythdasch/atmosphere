987743ba24d262ce67e8db4ca616e740a149230b
Scheck <david@kwyk.fr>
2013-09-17 15:29:58 +0200

    cleaning migrations gallery

--- gallery/migrations/0001_initial.py
+++ gallery/migrations/0001_initial.py
@@ -12,7 +12,11 @@ class Migration(SchemaMigration):
         db.create_table(u'gallery_author', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('name_fr', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
+            ('name_en', self.gf('django.db.models.fields.CharField')(max_length=100, null=True, blank=True)),
             ('biography', self.gf('django.db.models.fields.TextField')()),
+            ('biography_fr', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('biography_en', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
             ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
         ))
         db.send_create_signal(u'gallery', ['Author'])
@@ -21,11 +25,21 @@ class Migration(SchemaMigration):
         db.create_table(u'gallery_gallery', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('name_fr', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('name_en', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=50)),
             ('description', self.gf('django.db.models.fields.TextField')()),
+            ('description_fr', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('description_en', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
             ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
             ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
             ('author', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['gallery.Author'])),
+            ('meta_keywords', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_keywords_fr', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_keywords_en', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_description', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_description_fr', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('meta_description_en', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
         ))
         db.send_create_signal(u'gallery', ['Gallery'])
 
@@ -55,18 +69,32 @@ class Migration(SchemaMigration):
         u'gallery.author': {
             'Meta': {'object_name': 'Author'},
             'biography': ('django.db.models.fields.TextField', [], {}),
+            'biography_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'biography_fr': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'name_fr': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
         },
         u'gallery.gallery': {
             'Meta': {'object_name': 'Gallery'},
             'author': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['gallery.Author']"}),
             'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'description': ('django.db.models.fields.TextField', [], {}),
+            'description_en': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'description_fr': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'meta_description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_description_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_description_fr': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_keywords': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_keywords_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'meta_keywords_fr': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'name_fr': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
         },
         u'gallery.image': {
